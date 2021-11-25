8c9f726346e3ca543794fa8b1b197a8d5a104846
Vitorsns <santosv701@gmail.com>
2021-11-24 23:11:32 -0300

    testando a primeira classe

--- src/main/java/com/vitornogueira/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/vitornogueira/cursomc/resources/CategoriaResources.java
@@ -1,8 +1,13 @@
 package com.vitornogueira.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vitornogueira.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -11,8 +16,20 @@ public class CategoriaResources {
 
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() { 
+		
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria  cat2 = new Categoria(2, "Escritório");
+		
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1); 
+		lista.add(cat2);
+		
+		
+		
+		return lista;
 	}
 	
 	
