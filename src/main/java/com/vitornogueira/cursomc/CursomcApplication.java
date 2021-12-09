19eebfe02e11bb9273464ab32923b33f2b70c2dc
Vitorsns <santosv701@gmail.com>
2021-12-09 12:56:46 -0300

    Nao precisar usar o insert para consultar o banco

--- src/main/java/com/vitornogueira/cursomc/CursomcApplication.java
+++ src/main/java/com/vitornogueira/cursomc/CursomcApplication.java
@@ -1,15 +1,40 @@
 package com.vitornogueira.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vitornogueira.cursomc.domain.Categoria;
+import com.vitornogueira.cursomc.repositories.CategoriaRepository;
 
 
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	
+    @Autowired
+	private CategoriaRepository categoriaRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática"); 
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
 	}
 
 }
